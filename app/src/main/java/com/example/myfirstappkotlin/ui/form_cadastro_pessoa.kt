0f03229df31a30c89c7fbdc467453d11d7483418
cainasanches <cainasanches@hotmail.com>
2024-07-14 16:22:42 -0300

    Implementado layou primeiro Form de Cadastro

---
+++ app/src/main/java/com/example/myfirstappkotlin/ui/form_cadastro_pessoa.kt
@@ -0,0 +1,12 @@
+package com.example.myfirstappkotlin.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myfirstappkotlin.R
+
+class form_cadastro_pessoa : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_form_cadastro_pessoa)
+    }
+}
